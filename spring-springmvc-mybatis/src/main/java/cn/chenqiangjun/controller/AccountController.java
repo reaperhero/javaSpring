1dff96498201e6dfbfc57a8471e5db3aa33b9a30
chenqiangjun <chenqiangjun@mudu.tv>
2020-05-30 11:08:02 +0800

    sprintmvc-test

--- spring-springmvc-mybatis/src/main/java/cn/chenqiangjun/controller/AccountController.java
+++ spring-springmvc-mybatis/src/main/java/cn/chenqiangjun/controller/AccountController.java
@@ -1,4 +1,16 @@
 package cn.chenqiangjun.controller;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/account")
 public class AccountController {
+
+    @RequestMapping("/findAll")
+    public String findAll(){
+        System.out.println("表现层：查询所有账户...");
+        return "list";
+    }
 }
