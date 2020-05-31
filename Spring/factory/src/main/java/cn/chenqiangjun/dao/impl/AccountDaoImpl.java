4d6e1e0588bc8c8bdac4bbadced8557aca9ccfe7
chenqiangjun <chenqiangjun@mudu.tv>
2020-05-31 22:54:24 +0800

    factory bean

---
+++ Spring/factory/src/main/java/cn/chenqiangjun/dao/impl/AccountDaoImpl.java
@@ -0,0 +1,14 @@
+package cn.chenqiangjun.dao.impl;
+
+import cn.chenqiangjun.dao.IAccountDao;
+
+/**
+ * 账户的持久层实现类
+ */
+public class AccountDaoImpl implements IAccountDao {
+
+    public  void saveAccount(){
+
+        System.out.println("保存了账户");
+    }
+}
